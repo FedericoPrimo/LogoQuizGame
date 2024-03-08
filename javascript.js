1681a21969d4997b1aaf099795401c203c0a40c8
Federico <federico.calz02@gmail.com>
2024-03-08 12:31:29 +0100

    Inizio del progetto

---
+++ javascript.js
@@ -0,0 +1,59 @@
+const fileImmagini = ['absolut_2.png', 'acura_2.png', 'adidas_2.png', 'adio_2.png', 'adobe_2.png', 'aero.png', 'airwalk_1.png', 'aiwa_3.png', 'alibaba.png', 'aljazeera_2.png', 'allianz_2.png', 'amazon_2.png', 'americanexpress_2.png', 'android_2.png', 'animalplanet_2.png', 'apple_4.png', 'aquafresh.png', 'ariel_2.png', 'asus_3.png', 'audi_3.png', 'avg_2.png', 'axa_2.png', 'bacardi_2.png', 'barbie_1.png', 'barclays_3.png', 'batman_4.png', 'bayer_2.png', 'beko_3.png', 'bic_2.png', 'billabong_2.png', 'blackberry_2.png', 'blizzardentertainment_2.png', 'bluetooth_2.png', 'bluray_2.png', 'bnp_paribas.png', 'bp_2.png', 'bridgestone_2.png', 'bulgari.png', 'bundesliga_2.png', 'burgerking_2.png', 'cadillac_2.png', 'camel.png', 'cartoonnetwork_2.png', 'casio_3.png', 'chesterfield.png', 'chevrolet_2.png', 'chevron_2.png', 'chrome_2.png', 'chupachups_3.png', 'cinemacity_3.png', 'cisco_2.png', 'cnbs_2.png', 'cnn_2.png', 'cocacola_2.png', 'colgate_3.png', 'columbia-pictures_2.png', 'columbia_2.png', 'converse_2.png', 'cornetto.png', 'counter_strike_2.png', 'credit_suisse.png', 'crocs_2.png', 'delmonte_3.png', 'dhl_3.png', 'dior.png', 'discovery_2.png', 'dolby.png', 'dominospizza_2.png', 'dove_2.png', 'dragonball_3.png', 'dreamworks_2.png', 'dropbox_2.png', 'drpepper_2.png', 'dunkindonuts_2.png', 'duracell_3.png', 'easports_2.png', 'easyjet_3.png', 'ebay_3.png', 'element_2.png', 'elf.png', 'epson.png', 'espn_2.png', 'esprit.png', 'f1_3.png', 'facebook_3.png', 'familyguy_3.png', 'fanta.png', 'fedex_2.png', 'felix.png', 'ferrari_2.png', 'fila_2.png', 'flickr_3.png', 'fritos.png', 'garmin_3.png', 'geox_3.png', 'googledrive_2.png', 'greenpeace_3.png', 'hardrockcafe_3.png', 'harvard_2.png', 'hbo_2.png', 'hellokitty_2.png', 'hermes_2.png', 'hilton_2.png', 'hotwheels_2.png', 'huawei_2.png', 'hugo-boss.png', 'ibm_2.png', 'ikea_2.png', 'instagram_2.png', 'intel_2.png', 'jackass_3.png', 'jaguar_2.png', 'jenga_3.png', 'juicy-fruit.png', 'kinder_2.png', 'kroger_3.png', 'ktm_3.png', 'lavazza_3.png', 'lincoln_2.png', 'lindt_3.png', 'linkedin_3.png', 'lonelyplanet_3.png', 'loreal_3.png', 'lotus_2.png', 'lucas_arts_2.png', 'lucky_strike.png', 'man.png', 'marlboro_2.png', 'mars_2.png', 'martini_3.png', 'marvel_3.png', 'maserati_2.png', 'mastercard_2.png', 'mazda_2.png', 'mcafee_2.png', 'mcdonalds_2.png', 'mercedes_2.png', 'metallica_3.png', 'michelin_2.png', 'microsoft_2.png', 'milky-way.png', 'minecraft_3.png', 'mitshubishi_2.png', 'monster-high_2.png', 'monster_4.png', 'mozillafirefox_2.png', 'mtv_2.png', 'nationalgeographic_1.png', 'nescafe_2.png', 'nestle_1.png', 'netflix_2.png', 'nhl.png', 'nike_2.png', 'nintendo_2.png', 'nirvana_2.png', 'nissan_3.png', 'nivea_2.png', 'nutella_3.png', 'nvidia_2.png', 'opel_2.png', 'opera_2.png', 'oracle_2.png', 'oralb_2.png', 'oreo_2.png', 'oriflame.png', 'palmolive.png', 'pantene_3.png', 'pepsi_2.png', 'philips_3.png', 'pinterest_3.png', 'pizzahut_3.png', 'playdoh_3.png', 'playskool_3.png', 'playstationportable_2.png', 'playstation_2.png', 'popcap_3.png', 'powerpoint.png', 'pringles_2.png', 'pritt_3.png', 'puma_2.png', 'ralphlauren_1.png', 'redbull_2.png', 'reddit_2.png', 'rexona_3.png', 'rolex_1.png', 'rollerblade_1.png', 'safari_2.png', 'samsung_3.png', 'sega_2.png', 'seven-eleven_2.png', 'shell_2.png', 'skoda_2.png', 'skype_3.png', 'snapchat_4.png', 'sony_3.png', 'speedo_1.png', 'spotify_1.png', 'sprite_2.png', 'starbucks_2.png', 'street-fighter_2.png', 'superman_4.png', 'suzuki_2.png', 'the-new-york-times.png', 'thenorthface_2.png', 'threem_3.png', 'timberland_2.png', 'time_3.png', 'tnt_3.png', 'tomtom_2.png', 'topgear_2.png', 'toshiba.png', 'tropicana_2.png', 'tumbir_2.png', 'twitter_2.png', 't_mobile.png', 'uncle_bens_2.png', 'unesco_3.png', 'unicef_2.png', 'unilever_2.png', 'vans_3.png', 'vimeo_1.png', 'visa_2.png', 'vodafone_2.png', 'waltdisney_1.png', 'wella_2.png', 'whatsapp_3.png', 'whirpool_3.png', 'wifi_3.png', 'wikipedia_2.png', 'wilson_2.png', 'winamp_1.png', 'windows.png', 'winterfresh.png', 'world-of-warcraft_2.png', 'wwf_2.png', 'yamaha_2.png', 'youtube_2.png', 'zippo_3.png', 'zte_3.png']
+
+document.getElementById("risposta").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        // Ottieni l'elemento immagine
+        let logo = document.getElementById("logo");
+        
+        // Ottieni l'URL dell'immagine
+        let urlLogo = logo.src;
+        
+        // Estrai il nome dell'immagine dall'URL
+        let nomeLogo = urlLogo.substring(urlLogo.lastIndexOf('/') + 1);
+        
+        // Stampa il nome dell'immagine
+        console.log("Nome del file:", nomeLogo);
+        
+        // Prendo il nome vero del logo per validare la risposta dell'utente
+        nomeLogo = nomeLogo.split('_');
+        let soluzione = nomeLogo[0];
+        
+        console.log("Nome del logo:", soluzione);
+
+        let risposta = document.getElementById("risposta").value;
+
+        // pulisco la risposta
+        document.getElementById("risposta").value = "";
+
+        console.log("Risposta dell'utente: " + risposta);
+
+        if(risposta == soluzione){
+            console.log("Esatto!");
+            cambiaLogo();
+        }
+        else{
+            console.log("No, riprova");
+        }
+        
+    }
+
+});
+
+function cambiaLogo(){
+    // Devo scegliere un immagine random da mostrare
+    var indiceRandom = Math.floor(Math.random() * fileImmagini.length);
+    var logoRandom = fileImmagini[indiceRandom];
+    
+    // Crea il percorso completo dell'immagine
+    var pathImmagine = "img/" + logoRandom;
+
+
+    let nuovoLogo = document.createElement("img")
+    nuovoLogo.id = "logo";
+    nuovoLogo.alt = "logo_corrente";
+    nuovoLogo.src = pathImmagine;
+    let vecchioLogo = document.getElementById("logo");
+
+    let container = document.getElementById("container");
+    container.replaceChild(nuovoLogo, vecchioLogo);
+}
